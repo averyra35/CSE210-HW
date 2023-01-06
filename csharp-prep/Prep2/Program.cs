82ac692fe4bd1fc6463eff74b9745f66dd576696
Avery Anderson <averyra35@byui.edu>
2023-01-05 17:13:28 -0700

    Finished Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+        string letter = "";
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80 && percent < 90)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70 && percent < 80)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60 && percent < 70)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else if (percent < 70)
+        {
+            Console.WriteLine("You failed the class, better luck next time.");
+        }
+
+
     }
 }
