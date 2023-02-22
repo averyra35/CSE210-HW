28dbb1526c03f5fc3b7476a09bc43a28d280a10f
Avery Anderson <averyra35@byui.edu>
2023-02-22 11:52:57 -0700

    Finished The Learning 4 Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a = new Assignment("Avery Anderson", "Long Division");
+        Console.WriteLine(a.GetSummary());
+
+        MathAssignment m = new MathAssignment("Hailey Anderson", "Multiplication", "8.4", "11-20");
+        Console.WriteLine(m.GetSummary());
+        Console.WriteLine(m.GetHomeworkList());
+
+        WritingAssignment w = new WritingAssignment("Colt Anderson", "Cultures Around the World", "The Middle East");
+        Console.WriteLine(w.GetSummary());
+        Console.WriteLine(w.GetWritingInformation());
     }
 }
