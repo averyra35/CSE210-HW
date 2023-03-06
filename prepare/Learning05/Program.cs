9daa54a3f202392ee00595fbecdb99b7d6e02185
Avery Anderson <averyra35@byui.edu>
2023-03-06 11:59:03 -0700

    Finished Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square sq = new Square("Blue", 8);
+        shapes.Add(sq);
+
+        Rectangle r = new Rectangle("Green", 4, 6);
+        shapes.Add(r);
+
+        Circle c = new Circle("Violet", 10);
+        shapes.Add(c);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
